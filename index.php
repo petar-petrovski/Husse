a1aba501c90f9bb35dee1747e0f91a506064270b
petar-petrovski <p.petrovski85@gmail.com>
2022-02-09 12:28:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="content">
+
+            <div class="top-container">
+            <h1>Hello <span style="font-weight:700">Master Name</span>,</h1>
+            <p>Welcome to Husse</p>
+            </div>
+
+            <div class="nav-container">
+                <div class="nav-title">
+                <p>TRAININGS <br>& COURSES</p>
+                <button class="glocal-btn">Global</button>
+                <button class="glocal-btn">Local</button>
+                </div>
+                <div class="search-box" id="search">
+                    <input type="search" name="" id="search-input" placeholder="Search Trainings & Courses">
+                    <!-- <img src="img/loupe.png" alt=""> -->
+                    <!-- <button id="search-btn"></button> -->
+                </div>
+            </div>
+
+            <div class="filter-container">
+                <div class="filter-by">Filter by category</div>
+                <div class="sort-by">Sort by</div>                
+            </div>
+    
+    <!-- Gird -->
+    <div class="grid-container">
+        
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/beagle.jpg" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn reminder-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+                <div class="col">
+            <img src="img/german-shepard.jpg" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/haskey.jpg" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/turkish.jpg" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/img-placeholder.png" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/img-placeholder.png" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/img-placeholder.png" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/img-placeholder.png" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        <!-- Video Card - START -->
+        <div class="col">
+            <img src="img/img-placeholder.png" alt="">
+                      
+            <div class="card-content">
+            <div class="card-category">
+                <p class="category-text">SALES & MARKETING</p>
+                <p class="category-text">22. 12. 2021</p>
+            </div>  
+            <h3 class="title">Cats diet and Husse wet food for cats longer title</h3>
+            <br>
+            <p>Cats diet and Husse wet food for cats
+short description  Cats diet and Husse wet food for cats short description ...</p>
+            <br>
+            <div class="card-buttons">
+                <button class="card-btn"><img src="img/attachment.png" alt=""></button>
+                <button class="card-btn" style="padding: 10px 36px;">Set Reminder</button>
+                <button class="card-btn"><img src="img/three-dots.png" alt=""></button>
+
+            </div>
+            </div>
+        </div>
+        <!-- Video Card - END -->
+        </div>
+    </div>
+    
+    <div class="sidebar">
+            <div class="top-container">
+            <h1>Hello <span style="font-weight:700">Master Name</span>,</h1>
+            <p>Welcome to Husse</p>
+            </div>
+    </div>
+    
+    </div>
+</body>
+</html>
