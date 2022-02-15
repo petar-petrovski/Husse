519b6991bedc7bf9c2330f82ca64e87893e79cc8
petar-petrovski <p.petrovski85@gmail.com>
2022-02-15 23:38:15 +0100

    small css changes

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ function filterCard(value) {
       }
     });
   }
+  
   /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
