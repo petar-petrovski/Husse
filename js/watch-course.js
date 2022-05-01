b4439d808b264d99c2c8fd968bd6d2fcd92d9a92
petar-petrovski <p.petrovski85@gmail.com>
2022-05-02 01:23:26 +0200

    Finilizing video controls

--- js/watch-course.js
+++ js/watch-course.js
@@ -36,14 +36,53 @@ video.addEventListener ('timeupdate', function(){
     }
     document.getElementById('play-time').innerHTML=x + ":" + y;
 })
-
-
-    var fullTime = video.duration;
-
+window.addEventListener('load', function() {
+    var fullTime = video.duration; 
     var minutes = Math.floor(fullTime / 60);   
     var seconds = Math.floor(fullTime - minutes * 60)
     var x = minutes < 10 ? "0" + minutes : minutes;
     var y = seconds < 10 ? "0" + seconds : seconds;
     document.getElementById('full-time').innerHTML=x + ":" + y;
-    console.log(fullTime);
+});
 
+window.SetVolume = function(val) {
+    var player = document.getElementById('play-screen');
+    player.volume = val / 100;
+};
+
+
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'vol-control') {
+    target = document.getElementById('vol-control')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
+function showVolume() {
+    var volumeBtn = document.querySelector('.volume');
+    volumeBtn.classList.toggle("hide");
+    document.querySelector('.play-time').classList.toggle("move-right");
+}
+// $(document).ready(function(){
+//     $("#sound").click(function(){
+//       $("#vol-control").toggleClass("hide");
+//     });
+//   });
+function fullscreen() {
+    var element = document.getElementById("play-screen");       
+    if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }  
+  }
